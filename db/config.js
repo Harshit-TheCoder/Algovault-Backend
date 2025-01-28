11fd3479027f906b7a8d69e9733e7872f1772c88
Harshit-TheCoder <harshitharlalka11@gmail.com>
2025-01-28 16:38:51 +0530

    code pushed

--- db/config.js
+++ db/config.js
@@ -1,3 +1,13 @@
+// import mongoose from "mongoose";
+
+// mongoose.connect("mongodb://localhost:27017/AlgoVault");
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/AlgoVault");
+const connectionString = "mongodb+srv://harshitharlalka11:157056724@employees.n0krdfl.mongodb.net/AlgoVault?retryWrites=true&w=majority&appName=Employees";
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
